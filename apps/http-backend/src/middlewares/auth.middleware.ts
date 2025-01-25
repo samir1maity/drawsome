a1a1cc94f2b4c1827771223cac7faf99df83929a
samir1maity <s879maity@gmail.com>
2025-01-26 01:32:32 +0530

    added a common backend package

--- apps/http-backend/src/middlewares/auth.middleware.ts
+++ apps/http-backend/src/middlewares/auth.middleware.ts
@@ -1,11 +1,13 @@
 import { NextFunction, Request, Response } from "express";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 export function authMiddleware(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  const token = req.headers["authorization"] || req.headers["Authorization"] || "";
+  const token =
+    req.headers["authorization"] || req.headers["Authorization"] || "";
 
   // decode token
   // how can structure the req object in express global d ts
