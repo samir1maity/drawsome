e74be902f9c0eff8992e13c43cd7ce9df9b1b399
samir1maity <s879maity@gmail.com>
2025-01-28 22:54:34 +0530

    added decode token at ws service

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,17 +1,39 @@
 import { WebSocketServer } from "ws";
+import jwt from 'jsonwebtoken'
 
 const wss = new WebSocketServer({ port: 8080 });
 
+function checkUser(token:string){
+  try {
+    const decoded = jwt.verify(token, 'drawsome123#@')
+
+    if(typeof decoded === 'string' || !decoded || !decoded.id){
+      return null
+    }
+
+    const userId = decoded.id 
+
+    return userId
+  } catch (error) {
+
+    if (error instanceof Error) {
+      console.error("Error while token decode proccess :", error.message);
+    } else {
+      console.error("Unknown error:", error);
+    }
+
+    return null
+  }
+}
+
 wss.on("connection", function connection(ws, request) {
 
-    // google what can we get from request object
+  const url = request?.url
+  const urlSearchParam = new URLSearchParams(url?.split('?')[1])
+  const token = urlSearchParam.get('token') || ""
 
-    // then from url take the query param
+  const userId =  checkUser(token)
 
-    // using this user token findout user name
-
-
-    //
   ws.on("message", () => {
     ws.send("hello!");
   });
